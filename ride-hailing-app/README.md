0a381e1f3aee5fbca2644815061565f76cdfe0c1
Shinde Anjali Parashuram <anjaliparashuram6@gmail.com>
2025-09-24 12:04:48 +0530

    Delete ride-hailing-app directory

--- ride-hailing-app/README.md
+++
@@ -1,3 +0,0 @@
-# Ride Hailing App - Behavioral Patterns Demo
-
-Run `mvn clean package` then `java -jar target/ride-hailing-app-1.0.0.jar`
--- ride-hailing-app/src/main/java/com/rideapp/Main.java
+++
@@ -1,6 +0,0 @@
-package com.rideapp;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Ride Hailing App Demo Running...");
-    }
-}
